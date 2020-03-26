b8bf514b03b1c61cfbdc706be15400547071dc11
itdev-lenovo <it1devops@gmail.com>
2020-03-26 20:16:45 +0530

    files added

---
+++ Level2/Controllers/NewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level2.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Add Docker support with Visual Studio and Run the API and the Server in Docker from Visual Studio and add Nuget packages and add migrations and add docker image for MSSQL server and deploy your WebAPI to a docker container
+            return View();
+        }
+    }
+}
